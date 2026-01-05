14b572e7a076e9d1a2529ea9f1361d6e51e420d4
Luiz Felipe <luizfelipesmaciel@gmail.com>
2026-01-05 19:19:23 -0300

    Add step function

---
+++ iam.tf
@@ -0,0 +1,42 @@
+resource "aws_iam_role" "on_file_created_handler" {
+  name = "${var.project-name}-sfn-on-file-created-handler-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "states.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "on_file_created_handler" {
+  name = "${var.project-name}-sfn-on-file-created-handler-policy"
+  role = aws_iam_role.on_file_created_handler.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid    = "VisualEditor0",
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ],
+        Resource = [
+          aws_s3_bucket.inbound_bucket.arn,
+          "${aws_s3_bucket.inbound_bucket.arn}/*",
+          aws_s3_bucket.processed_data_bucket.arn,
+          "${aws_s3_bucket.processed_data_bucket.arn}/*"
+        ]
+      }
+    ]
+  })
+}
