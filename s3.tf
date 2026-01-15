4a559f5e51be497fca4fe5aa1e396845c6a46f9c
Luiz Felipe <luizfelipesmaciel@gmail.com>
2026-01-15 02:45:47 -0300

    Add glue scripts bucket

--- s3.tf
+++ s3.tf
@@ -10,3 +10,7 @@ resource "aws_s3_bucket_notification" "on_file_created" {
   bucket      = aws_s3_bucket.inbound_bucket.id
   eventbridge = true
 }
+
+resource "aws_s3_bucket" "glue_scripts" {
+  bucket = "${var.project-name}-glue-scripts-${local.account_id}-${local.region}"
+}
