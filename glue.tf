0f77744ce3cbe14dbcca18f2f961dea814af79e5
Luiz Felipe <luizfelipesmaciel@gmail.com>
2026-01-15 03:14:50 -0300

    Add glue job

---
+++ glue.tf
@@ -0,0 +1,33 @@
+resource "aws_glue_job" "process_data" {
+  name     = "${var.project-name}-process-data"
+  role_arn = aws_iam_role.glue_process_data.arn
+
+  glue_version      = "5.0"
+  number_of_workers = 2
+  worker_type       = "G.1X"
+
+  execution_property {
+    max_concurrent_runs = 3
+  }
+
+  command {
+    name            = "glueetl"
+    script_location = "s3://${aws_s3_object.process_data.bucket}/${aws_s3_object.process_data.key}"
+    python_version  = "3"
+  }
+
+  default_arguments = {
+    "--job-language"                     = "python"
+    "--enable-continuous-cloudwatch-log" = "true"
+    "--enable-metrics"                   = "true"
+    "--enable-spark-ui"                  = "true"
+    "--spark-event-logs-path"            = "s3://${aws_s3_bucket.glue_scripts.id}/spark-logs/"
+    "--TempDir"                          = "s3://${aws_s3_bucket.glue_scripts.id}/temp/"
+    "--REF_DATE"                         = ""
+    "--DOC_TYPE"                         = ""
+    "--CONTEXT_ID"                       = ""
+  }
+
+  max_retries = 1
+  timeout     = 60
+}
--- iam.tf
+++ iam.tf
@@ -1,3 +1,60 @@
+resource "aws_iam_role" "glue_process_data" {
+  name = "${var.project-name}-glue-process-data-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "glue.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "glue_process_data" {
+  name = "${var.project-name}-glue-process-data-policy"
+  role = aws_iam_role.glue_process_data.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid    = "VisualEditor0",
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:ListBucket"
+        ],
+        Resource = [
+          aws_s3_bucket.inbound_bucket.arn,
+          "${aws_s3_bucket.inbound_bucket.arn}/*",
+          aws_s3_bucket.processed_data_bucket.arn,
+          "${aws_s3_bucket.processed_data_bucket.arn}/*",
+          aws_s3_bucket.glue_scripts.arn,
+          "${aws_s3_bucket.glue_scripts.arn}/*",
+        ]
+      },
+      {
+        Sid    = "VisualEditor1",
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "*"
+      },
+    ]
+  })
+}
+
 resource "aws_iam_role" "on_file_created_handler" {
   name = "${var.project-name}-sfn-on-file-created-handler-role"
 
