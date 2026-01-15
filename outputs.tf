b7c58fd1c4247b06f075cb797bb5f84ef89062fa
Luiz Felipe <luizfelipesmaciel@gmail.com>
2026-01-15 01:14:01 -0300

    Clear bucket in test

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
-output "inbound_bucket" {
+output "inbound_bucket_name" {
   value = aws_s3_bucket.inbound_bucket.id
+}
+
+output "processed_data_bucket_name" {
+  value = aws_s3_bucket.processed_data_bucket.id
 }
--- test.sh
+++ test.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-INBOUND_BUCKET_NAME=$(jq -r '.inbound_bucket.value' outputs.json)
+INBOUND_BUCKET_NAME=$(jq -r '.inbound_bucket_name.value' outputs.json)
+PROCESSED_DATA_BUCKET_NAME=$(jq -r '.processed_data_bucket_name.value' outputs.json)
+
+aws s3 rm s3://$PROCESSED_DATA_BUCKET_NAME --recursive
+aws s3 rm s3://$INBOUND_BUCKET_NAME --recursive
+
 for filename in ./tests/*; do
     aws s3 cp $filename s3://$INBOUND_BUCKET_NAME/inbound_data/${filename:2}
 done
